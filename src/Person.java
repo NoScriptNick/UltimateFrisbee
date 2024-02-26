0deef6679a595d64f0fbdedf6ce8aa85ead26599
NoScriptNick <144457708+NoScriptNick@users.noreply.github.com>
2024-02-26 13:54:54 -0500

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,5 +1,30 @@
 public class Person {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    //variables
+    private String firstName;
+    private String lastName;
+
+    //constructor
+    public Person(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    //Accessors (Getters)
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public String toString(){
+        return "Name: " + getLastName() + " " + getFirstName();
+    }
+
+    //Mutators
+    public int throwDisc(int pow){
+        return 2*pow;
     }
 }
