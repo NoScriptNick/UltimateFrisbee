4d39005581b4bd459fbb5abce49e697f84f7e92e
NoScriptNick <144457708+NoScriptNick@users.noreply.github.com>
2024-02-26 20:39:35 -0500

    Update UltimateTeam.java

--- src/UltimateTeam.java
+++ src/UltimateTeam.java
@@ -9,29 +9,32 @@ public class UltimateTeam {
         Coach c1 = new Coach("Mr", "Boolean", "Head Coach");
         coaches.add(c1);
 
-        UltimatePlayer p1 = new UltimatePlayer("ss", "Dumb A", "Cutter", 0);
+        UltimatePlayer p1 = new UltimatePlayer("Emory", "George", "Cutter", 0);
         Captain p2 = new Captain("Nick", "Beres", "Handler", 9, true);
         players.add(p1);
         players.add(p2);
+        getCutters(players);
+    }
 
-        String getCutters(){
-            for (UltimatePlayer p: players) {
-                if (p.getPosition().equals("Cutter")) {
-                    return players(p).getFirstName();
-                }
+    public static void getCutters(ArrayList<UltimatePlayer> players) {
+        for (UltimatePlayer p : players) {
+            if (p.getPosition().equals("Cutter")) {
+                System.out.println(p);
             }
         }
+    }
 
-        String getHandlers(){
-            for (UltimatePlayer p: players) {
-                if (p.getPosition().equals("Handler")) {
-
-                }
+    public static void getHandlers(ArrayList<UltimatePlayer> players) {
+        for (UltimatePlayer p : players) {
+            if (p.getPosition().equals("Handler")) {
+                System.out.println(p);
             }
         }
+    }
 
-        public String toString(){
-
-        }
+    public String toString(ArrayList<UltimatePlayer> players){
+        String s = "";
+        s += super.toString();
+        return s;
     }
 }
